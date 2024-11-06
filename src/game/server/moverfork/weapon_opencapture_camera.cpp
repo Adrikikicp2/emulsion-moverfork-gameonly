c8980a45055a3c4a92cf2a5eff2cffe1e3662f60
Dr Peeper <pepperpr86@gmail.com>
2024-11-06 17:20:56 +0700

    added emulsion demo maps

--- src/game/server/moverfork/weapon_opencapture_camera.cpp
+++ src/game/server/moverfork/weapon_opencapture_camera.cpp
@@ -11,6 +11,7 @@ public:
 
 	void PrimaryAttack();
 	void SecondaryAttack();
+	virtual void Equip(CBaseCombatCharacter* pOwner);
 private:
 
 };
@@ -23,7 +24,8 @@ void CWeaponOpenCaptureCamera::SecondaryAttack() {
 
 }
 
-LINK_ENTITY_TO_CLASS(weapon_camera, CWeaponOpenCaptureCamera)
+LINK_ENTITY_TO_CLASS(weapon_opencapture_camera, CWeaponOpenCaptureCamera);
+PRECACHE_WEAPON_REGISTER(weapon_opencapture_camera);
 
 void CWeaponOpenCaptureCamera::Spawn() {
 
@@ -34,6 +36,14 @@ void CWeaponOpenCaptureCamera::Spawn() {
 	//SetNextThink(gpGlobals->curtime + 0.0001f);
 }
 
+void CWeaponOpenCaptureCamera::Equip(CBaseCombatCharacter* pOwner) {
+
+	BaseClass::Equip(pOwner);
+}
+
 void CWeaponOpenCaptureCamera::Precache() {
-	VScriptRunScript("opencapture/opencapture", true);
+	VScriptRunScript("opencapture", true);
+	PrecacheModel("models/props_opencapture/v_camera.mdl");
+	PrecacheModel("models/props_opencapture/v_camera.mdl");
+	BaseClass::Precache();
 }
